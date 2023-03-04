16b3384617ee636939f0ef6c871f4b6dc8f597a0
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-04 11:46:47 +0000

    1

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-problem_list = [
-  "Living Giant Tree", "Alien Flying Saucer",
-  "Monster Spirit from the Parallel Universe", "Evil Artificial Intelligence",
-  "Parasites That Capture the Brain", "Mutant Centipede", "Mad Godzilla",
-  "Black Dragon", "Titanium"
-]
+problem_list = ["Living Giant Tree", "Alien Flying Saucer","Monster Spirit from the Parallel Universe", "Evil Artificial Intelligence","Parasites That Capture the Brain", "Mutant Centipede", "Mad Godzilla","Black Dragon", "Titanium"]
